a5e587f90b143a00869e438adf4d9787d0b3c2d9
Eoghan Murray <eoghan@getthere.ie>
2012-06-16 16:13:34 +0100

    Pick up on font sizes and convert to an appropriate h1/h2/h3.. tag

--- sanitize-doc-html.py
+++ sanitize-doc-html.py
@@ -2,7 +2,7 @@
 from bs4 import BeautifulSoup, Comment
 import os, sys, optparse, codecs
 
-valid_tags = 'p i em b strong blockquote a h1 h2 h3 h4 pre br img ul ol li'.split()
+valid_tags = 'p i em b strong blockquote a h1 h2 h3 h4 h5 h6 pre br img ul ol li'.split()
 valid_attrs = 'href src'.split()
 
 def main():
@@ -19,7 +19,10 @@ def main():
     for comment in soup.findAll(
         text=lambda text: isinstance(text, Comment)):
         comment.extract()
+    font_sizes = sorted(set([int(font_tag.attrs['size']) for font_tag in soup.findAll(name='font') if 'size' in font_tag.attrs]))
     for tag in soup.find_all(True):
+        if tag.name == 'font' and 'size' in tag.attrs:
+            tag.name = 'h' + str(int(((font_sizes.index(int(tag.attrs['size']))) / float(len(font_sizes))) * 6)+1)
         if tag.name not in valid_tags:
             tag.hidden = True
         tag.attrs = dict((attr, val) for attr, val in tag.attrs.items() if attr in valid_attrs)
